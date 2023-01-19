be45c599b739fb3d42f7f84222e10459f58182df
ADDYMODHAVE <118976941+ADDYMODHAVE@users.noreply.github.com>
2023-01-19 08:44:35 +0530

    Update working.js

--- working.js
+++ working.js
@@ -21,6 +21,8 @@ showonscreen(obj);
 }
 
 function showonscreen(e){
+    document.getElementById("detail").value="";
+    document.getElementById("notes").value="";
 
     var li=`<li id=${e.notes}>Shopping Price:${e.price}--The Platform:${e.category}
     <input type="button" onClick="editbtn('${e.price}','${e.notes}','${e.category}')" VALUE="edit" style="background-color: yellow;">  <input type="button" onClick="deletebtn('${e.notes}','${e.price}')" value="X" style="background-color: yellow;"></li>`;
@@ -28,14 +30,15 @@ function showonscreen(e){
     add.innerHTML+=li;
     var value=parseInt(e.price);
     totalsum(value);
-    document.getElementById("detail").value="";
-    document.getElementById("notes").value="";
+    
    
 }
 function deletescreen(e){
     var parent=document.getElementById("list");
     var child=document.getElementById(e);
+    if(child){
     parent.removeChild(child);
+    }
 }
 function deletebtn(e,e1){
    
@@ -49,19 +52,22 @@ function editbtn(price,notes,category){
     document.getElementById("notes").value=notes;
     document.getElementById("category").value=category
     deletebtn(notes,price);
-    deletescreen(notes);
+    
 }
 function totalsum(total){
+    
      add=add+total;
    const calculator=document.getElementById("sum");
    const budget=document.getElementById("Budget");
    const li2=`<li id="total">The Total Cost:${add}</li>`;
    const li3=`<li id="total1">OUT OF BUDGET</li>`;
-   const li4=`<li id="total2" style="color: green;">NOT OUT OF BUDGET</li>`;
+   const li4=`<li id="total1" style="color: green;">NOT OUT OF BUDGET</li>`;
    calculator.innerHTML=li2;
    if(add == 0){
-    calculator.removeChild(li2);
-   }
+    budget.removeChild(document.getElementById("total1"));
+     calculator.removeChild(document.getElementById("total"));
+      }
+
    if(add<=10000){
     budget.innerHTML=li4;
    }
@@ -75,11 +81,21 @@ function minus(total){
 }
 
 function removealldata(){
-    location.reload(true);
     var localstorage=localStorage;
     var key=Object.keys(localstorage);
     key.forEach((e)=>{
     localStorage.removeItem(e);
     })
     alert("ALL CLEARED");
+    location.reload(true);
 }
+window.addEventListener("DOMContentLoaded",()=>{
+    console.log("success");
+    var dom=localStorage;
+    var domkey=Object.keys(dom);
+    domkey.forEach((key)=>{
+        var detailfordom=JSON.parse(dom.getItem(key));
+        showonscreen(detailfordom);
+    })
+
+})
